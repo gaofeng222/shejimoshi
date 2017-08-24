962bf50eca0a07158d97f3243d458db7865638da
gaofeng222 <596642721@qq.com>
2017-08-24 15:21:02 +0800

    this is a first commit

---
+++ chap1.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Administrator on 2017/8/24 0024.
+ */
+//灵活的语言
+/*
+    最基本的写法
+ */
+
+function checkName(){
+    //........
+}
+
+function checkEmail(){
+    //.........
+}
+function checkPassword(){
+    //.........
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
