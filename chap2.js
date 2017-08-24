452ff4e1c31ca40afe61ae6f360ce012fda4c266
gaofeng222 <596642721@qq.com>
2017-08-24 17:26:18 +0800

    this is a chap2 commit

--- chap2.js
+++ chap2.js
@@ -24,12 +24,32 @@ Book.prototype = {
 var book = new Book(10,"Javascript 设计模式",50);
 console.log(book.bookname)
 
+var Book1 = function(id,name,price){
+    var num = 1;
+    function checkId(){};
+    this.getName = function(){};
+    this.getPrice = function(){};
+    this.setName = function(){};
+    this.setPrice = function(){};
+    this.id = id;
+    this.copy = function(){}
+    this.setName(name);
+    this.setPrice(price);
+}
+Book.isChinese = true;
+Book.resetTime = function(){
+    console.log('new Time')
+}
+Book1.prototype ={
+    isJSBook : false,
+    display : function(){}
+}
 
-
-
-
-
-
+var b = new Book1(11,"Javascript设计模式",40);
+console.log(b.num)
+console.log(b.isJSBook)
+console.log(b.id)
+console.log(b.isChinese)
 
 
 
