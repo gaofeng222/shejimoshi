7047a944782c66a3238ee06c24e6dfeb7873613e
gaofeng222 <596642721@qq.com>
2017-08-24 17:01:26 +0800

    this is a chap2 commit

--- chap2.js
+++ chap2.js
@@ -4,13 +4,25 @@
 
 //面向对象编程
 
+//创建一个类
+var Book = function(id,bookname,price){
+    this.id = id;
+    this.bookname = bookname;
+    this.price = price;
+}
 
+/*Book.prototype.display = function(){
 
+}*/
 
+Book.prototype = {
+    display : function(){
 
+    }
+}
 
-
-
+var book = new Book(10,"Javascript 设计模式",50);
+console.log(book.bookname)
 
 
 
