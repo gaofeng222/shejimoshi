405ce76ae7a0c2557f1825522eeb08cc77537c5e
gaofeng222 <596642721@qq.com>
2017-08-25 15:31:28 +0800

    This a new testing on 8.25-331

--- demo2-3.js
+++ demo2-3.js
@@ -72,6 +72,8 @@ console.log(instance3.books)
 console.log(instance4.books)
 
 //组合式继承
+//在子类构造函数中执行父类构造函数
+//在子类原型上实例化父类
 
 function SuperClasses(name){
     this.name = name;
@@ -87,46 +89,116 @@ function SubClasses(name,time){
     this.time = time;
 }
 
+SubClasses.prototype = new SuperClasses();
+SubClasses.prototype.getTime = function(){
+    console.log(this.time)
+}
+
+
+var instancees = new SubClasses("JSbook",2000)
+instancees.books.push("设计模式2222")
+console.log(instancees.books)
+
+instancees.getName();
+instancees.getTime()
+
+
+var instancees2 = new SubClasses("css book",2004);
+console.log(instancees2.books)
+instancees2.getName();
+instancees2.getTime()
+
+
+//原型继承模式
+
+function inheritObject(o){
+    function F(){}
+    F.prototype = o;
+    return new F();
+}
+
+var book = {
+    name : 'js book',
+    alikeBook : ['css book','js book']
+}
+
+var newBook = inheritObject(book);
+newBook.name = 'ajax book';
+newBook.alikeBook.push("xml book");
+
+var otherBook = inheritObject(book);
+otherBook.name = 'flash book';
+otherBook.alikeBook.push("as book");
+
+console.log(newBook.name)
+console.log(newBook.alikeBook)
+console.log(otherBook.name)
+console.log(otherBook.alikeBook)
+console.log(book.name)
+console.log(book.alikeBook)
 
 
 
+//寄生式继承
+
+var book2 = {
+    name : "js book",
+    alikeBook : ["css book","html book"]
+}
+
+function createBook(obj){
+    var o = new inheritObject(obj)
+    o.getName = function(){
+        console.log(this.name); //教程这里有问题
+    }
+    return o;
+}
+
+var test = createBook(book2);
+test.name = "php book222";
+console.log(test.name)
+test.getName()
 
 
+/*
+    寄生组合式继承
+    传递参数 subClass 子类
+    传递参数 superClass 父类
+ */
 
+function inheritPrototype(subClass,superClass) {
+    var p = inheritObject(superClass.prototype)
+    p.constructor = subClass;
+    subClass.prototype = p;
+}
 
+function SuperClasses1(name){
+    this.name = name;
+    this.colors =["red","blue","red"];
+}
 
+SuperClasses1.prototype.getName = function(){
+    console.log(this.name);
+}
+function SubClasses1(name,time){
+    SuperClasses1.call(this,name);
+    this.time = time;
+}
 
+inheritPrototype(SubClasses1,SuperClasses1);
+SubClasses1.prototype.getTime = function(){
+    console.log(this.time);
+}
+var test2 = new SubClasses1("linux book",2016);
+var test3 = new SubClasses1("Mongo book",2012);
 
+test2.colors.push("yellow")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(test2.colors)
+console.log(test3.colors)
+test2.getName()
+test2.getTime()
+test3.getName()
 
 
 
