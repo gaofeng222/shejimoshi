9b022e245e8bb3110b9bf375da5b5d1331c41b70
gaofeng222 <596642721@qq.com>
2017-08-25 11:40:46 +0800

    This a new testing on 8.25-1050

--- demo2-3.js
+++ demo2-3.js
@@ -12,7 +12,7 @@ SuperClass.prototype.getSuperValue = function(){
 }
 
 function SubClass(){
-    this.subClass = false;
+    this.subValue = false;
 }
 //继承父类
 SubClass.prototype = new SuperClass()
@@ -23,6 +23,85 @@ SubClass.prototype.getSubValue = function(){
 
 var instance = new SubClass();
 console.log(instance.getSuperValue())
+console.log(instance.getSubValue())
+
+console.log( instance instanceof SuperClass  )
+console.log( instance instanceof SubClass  )
+console.log( SubClass instanceof SuperClass )
+console.log( SubClass.prototype instanceof SuperClass )
+console.log( instance instanceof Object)
+
+
+//类式继承的缺点
+//1.污染了其他实例的属性
+//2.无法传递参数
+
+function SuperClass1(){
+    this.books = [ 'Javascript','html','css' ];
+}
+function SubClass(){}
+SubClass.prototype = new SuperClass1();
+var instance1 = new SubClass();
+var instance2 = new SubClass();
+console.log(instance1.books)
+
+instance1.books.push("shejimoshi");
+console.log(instance2.books)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
