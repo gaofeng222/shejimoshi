35aca0d123f47ce9252ccf66b59f094c69fe4c12
gaofeng222 <596642721@qq.com>
2017-08-25 11:00:02 +0800

    This a new testing on 8.25-1050

--- demo2-3.js
+++ demo2-3.js
@@ -7,15 +7,22 @@ function SuperClass(){
     this.superValue = true;
 }
 
+SuperClass.prototype.getSuperValue = function(){
+    return this.superValue;
+}
 
+function SubClass(){
+    this.subClass = false;
+}
+//继承父类
+SubClass.prototype = new SuperClass()
+//为子类添加共有方法
+SubClass.prototype.getSubValue = function(){
+    return this.subValue;
+}
 
-
-
-
-
-
-
-
+var instance = new SubClass();
+console.log(instance.getSuperValue())
 
 
 
